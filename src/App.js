bebd669ce5692b3da12acd5fe1a7e991c93f9480
ChanRithy27 <chanrithy27@icloud.com>
2023-07-06 15:22:44 -0700

    Imported BrowserRouter into my App.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import {
+  BrowserRouter,
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <h1> Routing Practice </h1>
+      </BrowserRouter>
     </div>
   );
 }
